e0361494c08a824e853716fc9b8d9dceb1874f0e
intelligentdnb <juandiarts@gmail.com>
2022-11-10 09:57:46 -0300

    feat/creating portfolio UI

---
+++ components/PortfolioCoin/styles.ts
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+
+    root: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: 50,
+        margin: 10,
+        marginVertical: 10
+    },
+    image: {
+        height: 50,
+        width: 50, 
+        marginRight: 10
+    },
+    left: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between"
+    },
+    name: {
+        fontWeight: "bold",
+        marginBottom: 5,
+    },
+    symbol: {
+        color: "#6b6b6b"
+    },
+    value: {
+        fontSize: 18,
+        fontWeight: "600",
+        marginBottom: 5,
+    },
+});
+
+export default styles;
--- screens/PortfolioScreen/index.tsx
+++ screens/PortfolioScreen/index.tsx
@@ -1,13 +1,48 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, FlatList } from "react-native";
+import PortfolioCoin from "../../components/PortfolioCoin";
+import styles from "./styles";
+const image = require("../../assets/images/Saly-10.png");
+const btcIMG = require("../.././src/imagesEJ/btc.png");
+const ethIMG = require("../.././src/imagesEJ/eth.png");
+const usdtIMG = require("../.././src/imagesEJ/usdt.png");
+
+
+const portfolioCoins = [{
+  id: "1",
+  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg",
+  name: "Dollar Thether",
+  symbol: "USD",
+  amount: 68.233,
+  valueUSD: 89233
+},{
+  id: "2",
+  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg",
+  name: "Bitcoin",
+  symbol: "USD",
+  amount: 1.12,
+  valueUSD: 19233
+},{
+  id: "3",
+  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg",
+  name: "Ethereum",
+  symbol: "USD",
+  amount: 8.12,
+  valueUSD: 22233
+}]
 
 const PortfolioScreen = () => {
 
-    return (
-      <View style={{flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text></Text>
+  return (
+    <View style={styles.root}>
+      <Image style={styles.image} source={image} />
+      <View style={styles.balanceContainer}>
+        <Text style={styles.label}>Portfolio balance</Text>
+        <Text style={styles.balance}>$69,857</Text>
       </View>
-    )
+      <FlatList style={{width: "100%"}} data={portfolioCoins} renderItem={({item}) => <PortfolioCoin portfolioCoin={item} key={item.id} />} />
+    </View>
+  )
 }
 
 export default PortfolioScreen;
